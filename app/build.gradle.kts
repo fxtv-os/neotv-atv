92ddb046cc52c95f3ddda14272be9f2e93b9bdc9
Jona Korves <jkndm@icloud.com>
2026-07-14 12:27:21 +0200

    Build: 2.0.1_genericandroidtvdevice (2026-07-14 12:27)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.3.9_genericandroidtvdevice"
+        versionName = "2.0.1_genericandroidtvdevice"
     }
 
     buildTypes {
